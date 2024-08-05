038903eaf0b12a51b2bb737f0e26031c7ce83f0a
MihaiL-R78 <Rastorgyev-MihaiL@yandex.ru>
2024-08-05 17:45:13 +0300

    Исправил ошибку StringComparison.java

--- StringComparison.java
+++ StringComparison.java
@@ -4,16 +4,16 @@ public class StringComparison {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Введите первое число: ");
-        int a = scanner.nextInt();
+        System.out.print("Введите первую строку: ");
+        String str1 = scanner.nextLine();
 
-        System.out.print("Введите второе число: ");
-        int b = scanner.nextInt();
+        System.out.print("Введите вторую строку: ");
+        String str2 = scanner.nextLine();
 
-        if (a == b) {
-            System.out.println("Числа идентичны");
+        if (str1.equals(str2)) {
+            System.out.println("Строки идентичны");
         } else {
-            System.out.println("Числа неидентичны");
+            System.out.println("Строки неидентичны");
         }
 
         scanner.close();
